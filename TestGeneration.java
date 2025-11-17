1c029a1f338a8c75cb07d2f6bfef3a45cce80b06
KevinT <98439580+Ktom0@users.noreply.github.com>
2025-11-17 13:38:55 -0500

    Update TestGeneration.java

--- TestGeneration.java
+++ TestGeneration.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import binary.Generation;
 import binary.GPTree;
 
@@ -6,24 +7,32 @@ public class TestGeneration {
 
     public static void main(String[] args) {
 
-        String fileName = args[0];
+        Scanner sc = new Scanner(System.in);
+        String fileName = sc.nextLine().trim();
+        sc.close();
 
         Generation gen = new Generation(500, 6, fileName);
 
         gen.evalAll();
 
         gen.printBestTree();
+
         gen.printBestFitness();
 
-        ArrayList<GPTree> top = gen.getTopTen();
+        ArrayList<GPTree> topTenTrees = gen.getTopTen();
 
         System.out.print("Top Ten Fitness Values: ");
-        for (int i = 0; i < top.size(); i++) {
-            System.out.printf("%.2f", top.get(i).getFitness());
-            if (i < top.size() - 1) {
+
+        for (int i = 0; i < topTenTrees.size(); i++) {
+            double fitnessValue = topTenTrees.get(i).getFitness();
+
+            System.out.printf("%.2f", fitnessValue);
+
+            if (i < topTenTrees.size() - 1) {
                 System.out.print(", ");
             }
         }
-        System.out.println();
+
+        System.out.println();  
     }
 }
